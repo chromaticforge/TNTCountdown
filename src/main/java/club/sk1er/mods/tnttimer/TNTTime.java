c124cfedde0ebb9c528350499cdcfec222028755
awruff <sillysys@proton.me>
2025-11-19 19:32:57 -0500

    wip: utilize generic nametag rendering
    so nametags mods (namely, polynametag) can modify the visuals.

--- src/main/java/club/sk1er/mods/tnttimer/TNTTime.java
+++ src/main/java/club/sk1er/mods/tnttimer/TNTTime.java
@@ -2,14 +2,7 @@ package club.sk1er.mods.tnttimer;
 
 import cc.polyfrost.oneconfig.utils.hypixel.HypixelUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.multiplayer.WorldClient;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.WorldRenderer;
-import net.minecraft.client.renderer.entity.RenderTNTPrimed;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.entity.item.EntityTNTPrimed;
 import net.minecraft.scoreboard.ScoreObjective;
 import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.util.EnumChatFormatting;
@@ -18,9 +11,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-import org.lwjgl.opengl.GL11;
 
-import java.awt.Color;
 import java.text.DecimalFormat;
 
 @Mod(modid = TNTTime.ID, name = TNTTime.NAME, version = TNTTime.VERSION)
@@ -28,9 +19,6 @@ public class TNTTime {
     public static final String ID = "@MOD_ID@";
     public static final String NAME = "@MOD_NAME@";
     public static final String VERSION = "@MOD_VERSION@";
-
-    @Mod.Instance
-    public static TNTTime INSTANCE;
 
     private final Minecraft mc = Minecraft.getMinecraft();
     private final DecimalFormat timeFormatter = new DecimalFormat("0.00");
@@ -45,9 +33,9 @@ public class TNTTime {
     @SubscribeEvent
     public void clientTick(TickEvent.ClientTickEvent event) {
         if (event.phase != TickEvent.Phase.START
-            // no need to check super often, they're likely not going to see tnt within 5 seconds
-            // of moving out of a bedwars game/lobby
-            || this.checkTimer++ < 250) {
+                // no need to check super often, they're likely not going to see tnt within 5 seconds
+                // of moving out of a bedwars game/lobby
+                || this.checkTimer++ < 250) {
             return;
         }
 
@@ -73,61 +61,6 @@ public class TNTTime {
         final ScoreObjective sidebarObjective = scoreboard.getObjectiveInDisplaySlot(1);
         if (sidebarObjective != null) {
             playingBedwars = EnumChatFormatting.getTextWithoutFormattingCodes(sidebarObjective.getDisplayName()).contains("BED WARS");
-        }
-    }
-
-    public void renderTag(RenderTNTPrimed tntRenderer, EntityTNTPrimed tntPrimed, double x, double y, double z, float partialTicks) {
-        // hypixel changes the fuse time in bedwars to explode around the 28th tick,
-        // which makes the value of the fuse starting timer presumably 52 instead of 80
-        // this can fluctuate between 27 and 28, but 28 seems to be more common, so we can sit with that instead
-        final int fuseTimer = this.playingBedwars ? tntPrimed.fuse - 28 : tntPrimed.fuse;
-        if (fuseTimer < 1) return;
-        double distance = tntPrimed.getDistanceSqToEntity(tntRenderer.getRenderManager().livingPlayer);
-
-        if (distance <= 4096D) {
-            float number = (fuseTimer - partialTicks) / 20F;
-            String time = this.timeFormatter.format(number);
-            FontRenderer fontrenderer = tntRenderer.getFontRendererFromRenderManager();
-            GlStateManager.pushMatrix();
-            GlStateManager.translate((float) x + 0.0F, (float) y + tntPrimed.height + 0.5F, (float) z);
-            GL11.glNormal3f(0.0F, 1.0F, 0.0F);
-            GlStateManager.rotate(-tntRenderer.getRenderManager().playerViewY, 0.0F, 1.0F, 0.0F);
-
-            int xMultiplier = 1; // Nametag x rotations should flip in front-facing 3rd person
-
-            if (mc.gameSettings.thirdPersonView == 2) {
-                xMultiplier = -1;
-            }
-
-            float scale = 0.02666667f;
-            GlStateManager.rotate(tntRenderer.getRenderManager().playerViewX * xMultiplier, 1.0F, 0.0F, 0.0F);
-            GlStateManager.scale(-scale, -scale, scale);
-            GlStateManager.disableLighting();
-            GlStateManager.depthMask(false);
-            GlStateManager.disableDepth();
-            GlStateManager.enableBlend();
-            GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-            Tessellator tessellator = Tessellator.getInstance();
-            WorldRenderer worldrenderer = tessellator.getWorldRenderer();
-            int stringWidth = fontrenderer.getStringWidth(time) >> 1;
-            // refer to the comment at the top of the method
-            float green = Math.min(fuseTimer / (this.playingBedwars ? 52 : 80f), 1f);
-            Color color = new Color(1f - green, green, 0f);
-            GlStateManager.enableDepth();
-            GlStateManager.depthMask(true);
-            GlStateManager.disableTexture2D();
-            worldrenderer.begin(7, DefaultVertexFormats.POSITION_COLOR);
-            worldrenderer.pos(-stringWidth - 1, -1, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-            worldrenderer.pos(-stringWidth - 1, 8, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-            worldrenderer.pos(stringWidth + 1, 8, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-            worldrenderer.pos(stringWidth + 1, -1, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-            tessellator.draw();
-            GlStateManager.enableTexture2D();
-            fontrenderer.drawString(time, -fontrenderer.getStringWidth(time) >> 1, 0, color.getRGB());
-            GlStateManager.enableLighting();
-            GlStateManager.disableBlend();
-            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-            GlStateManager.popMatrix();
         }
     }
 }
--- src/main/java/club/sk1er/mods/tnttimer/mixins/RenderTNTPrimedMixin.java
+++ src/main/java/club/sk1er/mods/tnttimer/mixins/RenderTNTPrimedMixin.java
@@ -1,6 +1,7 @@
 package club.sk1er.mods.tnttimer.mixins;
 
-import club.sk1er.mods.tnttimer.TNTTime;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderTNTPrimed;
 import net.minecraft.entity.item.EntityTNTPrimed;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,12 +10,18 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderTNTPrimed.class)
-public class RenderTNTPrimedMixin {
+public abstract class RenderTNTPrimedMixin extends Render<EntityTNTPrimed> {
+    protected RenderTNTPrimedMixin(RenderManager renderManager) {
+        super(renderManager);
+    }
+
     @Inject(
             method = "doRender(Lnet/minecraft/entity/item/EntityTNTPrimed;DDDFF)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/Render;doRender(Lnet/minecraft/entity/Entity;DDDFF)V")
     )
     private void drawNametag(EntityTNTPrimed entity, double x, double y, double z, float entityYaw, float partialTicks, CallbackInfo ci) {
-        TNTTime.INSTANCE.renderTag(((RenderTNTPrimed)(Object)this), entity, x, y, z, partialTicks);
+//        TNTTime.INSTANCE.renderTag(((RenderTNTPrimed)(Object)this), entity, x, y, z, partialTicks);
+
+        renderLivingLabel(entity, "Fuse", x, y, z, 64);
     }
 }
